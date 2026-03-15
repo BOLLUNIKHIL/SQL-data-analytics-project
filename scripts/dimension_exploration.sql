2ad630b7de8daf86f983cc756a008961f496f6ef
BOLLU NIKHIL <84654902+BOLLUNIKHIL@users.noreply.github.com>
2026-03-15 20:26:06 +0100

    Update dimension_exploration.sql
    new sql quries

--- scripts/dimension_exploration.sql
+++ scripts/dimension_exploration.sql
@@ -1,1 +1,8 @@
+-- displays the different types of countries 
+SELECT DISTINCT country FROM gold.dim_customers;
 
+-- displays different types of category & subcategory
+SELECT DISTINCT category,subcategory FROM gold.dim_products;
+
+-- displays all distinct order_dates in order 
+SELECT DISTINCT order_date FROM gold.fact_sales ORDER BY order_date;
