c9afb0d5577adf9abe3a2e00b5adcd8b3385e21f
BOLLU NIKHIL <84654902+BOLLUNIKHIL@users.noreply.github.com>
2026-03-15 21:28:32 +0100

    Update date_exploration.sql
    explored date timeline

--- scripts/date_exploration.sql
+++ scripts/date_exploration.sql
@@ -1,1 +1,14 @@
+-- displays the latest and oldest orders and orders timeline
+SELECT 
+	MIN(order_date) AS oldest_order,
+	MAX(order_date) AS latest_order,
+	DATEDIFF(year,MIN(order_date),MAX(order_date)) AS order_timeline
+FROM gold.fact_sales;
 
+-- displays the youngest and oldest age and 
+SELECT 
+	MIN(birthdate) AS oldest,
+	DATEDIFF(year,MIN(birthdate),GETDATE()) AS oldest_age,
+	MAX(birthdate) AS youngest,
+	DATEDIFF(year,MAX(birthdate),GETDATE()) AS youngest_age
+FROM gold.dim_customers  where birthdate is not null  
